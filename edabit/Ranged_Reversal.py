fea30b1be2acfa7791aa6e8c57b51426f0cc13cd
kakyra <suin-i@mail.ru>
2020-12-17 17:03:39 -0600

    edited Ranged_reversal file

--- edabit/Ranged_Reversal.py
+++ edabit/Ranged_Reversal.py
@@ -1,1 +1,4 @@
-print("hello world")
+def ranged_reversal(lst, start, finish):
+    return lst[:start] + lst[start:finish+1][::-1] + lst[finish+1:]
+
+print(ranged_reversal([1, 2, 3, 4, 5, 6], 1, 3))
